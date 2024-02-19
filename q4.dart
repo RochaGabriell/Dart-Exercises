047458f1579c5b5b633a3b841a1851634f922d0a
RochaGabriell <rochagabriell.dev@gmail.com>
2024-02-19 19:50:25 -0300

    Finish

--- q4.dart
+++ q4.dart
@@ -6,3 +6,54 @@ ordene os alunos, de forma crescente, pela nota 1, e liste-os. Finalmente, consi
 para ser aprovado o aluno deve ter no mínimo média 7, liste, em ordem alfabética, os
 alunos reprovados.
 */
+
+import 'dart:io';
+
+class Aluno {
+  String nome;
+  double nota1;
+  double nota2;
+
+  Aluno(this.nome, this.nota1, this.nota2);
+
+  double calcularMediaPonderada() {
+    return (nota1 * 2 + nota2 * 3) / 5;
+  }
+}
+
+void main() {
+  List<Aluno> alunos = [];
+
+  for (int i = 0; i < 8; i++) {
+    print('Cadastro do aluno ${i + 1}:');
+    print('Nome: ');
+    String nome = stdin.readLineSync()!;
+    print('Nota 1: ');
+    double nota1 = double.parse(stdin.readLineSync()!);
+    print('Nota 2: ');
+    double nota2 = double.parse(stdin.readLineSync()!);
+
+    alunos.add(Aluno(nome, nota1, nota2));
+  }
+
+  alunos.sort((a, b) => b.calcularMediaPonderada().compareTo(a.calcularMediaPonderada()));
+
+  print('Alunos ordenados pela média ponderada:');
+  alunos.forEach((aluno) {
+    print('${aluno.nome}: ${aluno.calcularMediaPonderada()}');
+  });
+
+  alunos.sort((a, b) => a.nota1.compareTo(b.nota1));
+
+  print('Alunos ordenados pela nota 1:');
+  alunos.forEach((aluno) {
+    print('${aluno.nome}: Nota 1 - ${aluno.nota1}, Nota 2 - ${aluno.nota2}');
+  });
+
+  List<Aluno> reprovados = alunos.where((aluno) => aluno.calcularMediaPonderada() < 7).toList();
+  reprovados.sort((a, b) => a.nome.compareTo(b.nome));
+  print('Alunos reprovados:');
+  reprovados.forEach((aluno) {
+    print('${aluno.nome}: ${aluno.calcularMediaPonderada()}');
+  });
+}
--- q5.dart
+++ q5.dart
@@ -5,3 +5,73 @@ encontre, verifique se está em uma posição par ou ímpar do vetor.
 a) usando busca sequencial;
 b) usando busca binária;
 */
+
+import 'dart:io';
+
+void main() {
+  List<int> numeros = [];
+  Set<int> numerosSet = {};
+
+  print('Digite 15 números diferentes:');
+  for (int i = 0; i < 15; i++) {
+    int numero;
+    do {
+      print('Número ${i + 1}:');
+      numero = int.parse(stdin.readLineSync()!);
+      if (numerosSet.contains(numero)) {
+        print('Número já cadastrado. Digite outro número.');
+      }
+    } while (numerosSet.contains(numero));
+    numeros.add(numero);
+    numerosSet.add(numero);
+  }
+
+  numeros.sort();
+
+  print('Números ordenados:');
+  print(numeros);
+
+  print('Digite um número para buscar no vetor:');
+  int numeroBusca = int.parse(stdin.readLineSync()!);
+  int posicaoSequencial = buscaSequencial(numeros, numeroBusca);
+  if (posicaoSequencial != -1) {
+    print('O número $numeroBusca foi encontrado na posição $posicaoSequencial (Índice: ${posicaoSequencial - 1}).');
+    print('Esta posição é ${posicaoSequencial % 2 == 0 ? 'par' : 'ímpar'} no vetor.');
+  } else {
+    print('O número $numeroBusca não foi encontrado no vetor.');
+  }
+
+  int posicaoBinaria = buscaBinaria(numeros, numeroBusca);
+  if (posicaoBinaria != -1) {
+    print('Usando busca binária, o número $numeroBusca foi encontrado na posição $posicaoBinaria (Índice: ${posicaoBinaria - 1}).');
+    print('Esta posição é ${posicaoBinaria % 2 == 0 ? 'par' : 'ímpar'} no vetor.');
+  } else {
+    print('Usando busca binária, o número $numeroBusca não foi encontrado no vetor.');
+  }
+}
+
+int buscaSequencial(List<int> lista, int valor) {
+  for (int i = 0; i < lista.length; i++) {
+    if (lista[i] == valor) {
+      return i + 1;
+    }
+  }
+  return -1;
+}
+
+int buscaBinaria(List<int> lista, int valor) {
+  int inicio = 0;
+  int fim = lista.length - 1;
+
+  while (inicio <= fim) {
+    int meio = ((inicio + fim) / 2).floor();
+    if (lista[meio] == valor) {
+      return meio + 1;
+    } else if (lista[meio] < valor) {
+      inicio = meio + 1;
+    } else {
+      fim = meio - 1;
+    }
+  }
+  return -1;
+}
